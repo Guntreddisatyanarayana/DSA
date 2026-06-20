ece022729ac6ed9b77afbfd48f033dce436866ed
Satyanarayana Guntreddi <satyaguntreddi8@gmail.com>
2026-06-20 10:37:00 +0530

    Time: 36 ms (5.34%) | Memory: 102.3 MB (6.73%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -3,12 +3,13 @@ class Solution {
         Arrays.sort(nums);
         List<Integer> L1=new ArrayList<>();
         HashSet<Integer> s1=new HashSet<>();
-        for (int num : nums) {
-    if (num > 0 && s1.add(num)) {
-        L1.add(num);
-    }
-}
+        for(int i=0;i<nums.length;i++){
+            if(!s1.contains(nums[i]) && nums[i]>0){
+                s1.add(nums[i]);
+                L1.add(nums[i]);
+            }
 
+        }
         if(L1.size()==0){
             return 1;
         }
@@ -16,6 +17,7 @@ class Solution {
           if(arr[0]>0 && arr[0]!=1){
             return 1;
         }
+
                 
     
         
